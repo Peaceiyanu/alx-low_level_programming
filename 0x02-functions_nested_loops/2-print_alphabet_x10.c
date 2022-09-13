427de4808255ff2d11acb9e62459eb0a3b44db96
Peaceiyanu <peaceiyanu3010@gmail.com>
2022-09-13 05:33:17 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -> prints the lowercase alphabets
+ */
+
+void print_alphabet_x10(void)
+{
+	int g;
+	int h;
+
+	for (g = 0; g < 10; g++)
+	{
+		for (h = 'a'; h <= 'z'; z++)
+		{
+			_putchar(g);
+		}
+		_putchar ('\n');
+	}
+}
