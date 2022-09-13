d69ea067ace8be41b640fe0778a646f8ab5cf1e0
Peaceiyanu <peaceiyanu3010@gmail.com>
2022-09-13 05:06:02 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet -> prints the lowercase alphabets
+ */
+
+void print_alphabet(void);
+{
+	int v;
+
+	for (v = 'a'; v <= 'z'; v++)
+	{
+		_putchar(v);
+	}
+	_putchar('\n');
+}
