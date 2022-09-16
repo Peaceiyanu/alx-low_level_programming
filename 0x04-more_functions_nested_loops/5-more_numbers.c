a5522c6e765e6c447520f05c32ed4e9f113b0a76
Peaceiyanu <peaceiyanu3010@gmail.com>
2022-09-15 17:09:24 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,19 +3,21 @@
 
 /**
  * more_numbers - prints numbers to 14
+ *
+ * Return: (0 - 14)x10
  */
 
 void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0, j < 15; j++)
+		for (j = 0, j <= 14; j++)
 		{
 			if (j >= 10)
-			_putchar(j / 10 + '0');
-			_putchar(j % 10 + '0');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
 	}
