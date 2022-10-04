5797e6f308a97921322a8a00527cdcd5a9e5378b
Peaceiyanu <peaceiyanu3010@gmail.com>
2022-10-04 11:18:52 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,10 +16,10 @@ char *str_concat(char *s1, char *s2)
 	int index, concat_index = 0, len = 0;
 
 	if (s1 == NULL)
-		s1 = " ";
+		s1 = "";
 
 	if (s2 == NULL)
-		s2 = " ";
+		s2 = "";
 
 	for (index = 0; s1[index] || s2[index]; index++)
 		len++;
@@ -30,6 +30,10 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 
 	for (index = 0; s1[index]; index++)
+		concat_str[concat_index++] = s1[index];
+
+	for (index = 0; s2[index]; index++)
 		concat_str[concat_index++] = s2[index];
+
 	return (concat_str);
 }
