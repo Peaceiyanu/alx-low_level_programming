883d9283ea96ace35421273816b32d8b1a9a0b0b
Peace Iyanu Joseph <110996772+Peaceiyanu@users.noreply.github.com>
2022-10-20 20:16:25 +0100

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,1 +1,13 @@
-hjbcjjnncsccnsbsddjhjhdkhiuhenshhsmnu
+#include <stdio.h>
+
+void __attribute__((constructor)) hare(void);
+
+/**
+ * hare - Prints a string before the
+ *        main function is executed.
+ */
+void hare(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
