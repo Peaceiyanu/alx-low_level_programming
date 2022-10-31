24ddb527bef29177415e5787332a0406d7b5dc30
Peaceiyanu <peaceiyanu3010@gmail.com>
2022-10-31 08:43:06 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -6,6 +6,7 @@
  * @text_content: added content.
  *
  * Return: 1 if the file exists. -1 if the fails does not exist
+ * or if it fails.
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
