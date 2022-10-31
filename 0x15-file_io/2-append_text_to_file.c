74e9e518a24c0e0f51817e7be4477acbcdd9e26e
Peace Iyanu Joseph <110996772+Peaceiyanu@users.noreply.github.com>
2022-10-31 16:47:50 +0100

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,8 +14,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	int nletters;
 	int rwr;
 
-
-	return (-1);
+	if (!filename)
+		return (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 
